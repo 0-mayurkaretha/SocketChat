0c5f57141ccb5d5e6b73574979a36bb9cb5158e7
0-mayurkaretha <0.mayurkaretha@gmail.com>
2024-01-23 17:19:43 +0530

    changes localhost to deployed endpoint

--- backend/server.js
+++ backend/server.js
@@ -53,7 +53,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000" || "http://localhost:5000",
+    origin: "https://socketchat-vfzo.onrender.com",
     // credentials: true,
   },
 });
